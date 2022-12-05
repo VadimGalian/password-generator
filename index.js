ad9cf0b80b47ba404223c99119492a9992b40789
VadimGalian <119458044+VadimGalian@users.noreply.github.com>
2022-12-05 21:16:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -95,23 +95,41 @@ const characters = [
 const generatePasswordEl = document.getElementById("gen_password");
 const password1El = document.getElementById("password1");
 const password2El = document.getElementById("password2");
-
+const passwordLengthEl = document.getElementById("password_length");
+const inputAlertEl = document.getElementById("input-alert");
 generatePasswordEl.addEventListener("click", getRandomPasswords);
 
 function getRandomPasswords() {
-  let randomPassword1 = generateRandomPassword();
-  let randomPassword2 = generateRandomPassword();
+  reset();
+  let length1 = passwordLengthEl.value;
+  let length2 = Math.floor(Math.random() * 16) + 5;
+
+  if (length1 > 20 || length1 < 5) {
+    passwordLengthEl.value = "";
+    passwordLengthEl.placeholder = "Incorrect password length";
+    inputAlertEl.style.display = "block"
+    return;
+  }
+
+  let randomPassword1 = generateRandomPassword(length1);
+  let randomPassword2 = generateRandomPassword(length2);
   password1El.textContent = randomPassword1;
   password2El.textContent = randomPassword2;
 }
 
-function generateRandomPassword() {
+function generateRandomPassword(length) {
   let password = "";
 
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < length; i++) {
     let rand = Math.floor(Math.random() * characters.length);
     password = password + characters[rand];
   }
 
   return password;
+}
+
+function reset() {
+  password1El.textContent = "";
+  password2El.textContent = "";
+  inputAlertEl.style.display = "none"
 }
